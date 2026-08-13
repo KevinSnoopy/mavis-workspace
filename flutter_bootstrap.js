bb9a5a3ef1c21c3aafde75f5d1bd75a79e51f76e
Mavis Agent <mavis@agent.local>
2026-08-13 00:00:41 +0000

    v1.2 - complete features

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1782166854"
+    serviceWorkerVersion: "1203783966"
   }
 });
